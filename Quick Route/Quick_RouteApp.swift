455555c07fe1edfeaadc5019d6a97cfe5b317db1
Nick Runje <nickrunje@gmail.com>
2025-02-04 19:07:55 -0800

    Initial Commit

---
+++ Quick Route/Quick_RouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quick_RouteApp.swift
+//  Quick Route
+//
+//  Created by Nicholas Runje on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Quick_RouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
