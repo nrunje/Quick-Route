151963ce164f52f3f72545ed91012bdfa5c8eccd
Nick Runje <nickrunje@gmail.com>
2025-03-19 13:25:42 -0700

    Gets user's location to display on map

--- Quick Route/Views/MapView.swift
+++ Quick Route/Views/MapView.swift
@@ -9,8 +9,31 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    @ObservedObject private var locationManager = LocationManager.shared
+    @State private var region = MKCoordinateRegion()
+    @State private var sheetOffset: CGFloat = 0
+    @State private var sheetHeight: CGFloat = 300 // Adjust this value as needed
+    
     var body: some View {
-        Text("This is a Map View")
+        ZStack {
+            // Map
+            Map(coordinateRegion: $region)
+                .ignoresSafeArea(edges: .top)
+                .onAppear() {
+                    print(locationManager.userLocation)
+                    
+                    if let location = locationManager.userLocation {
+                        region = MKCoordinateRegion(
+                            center: location.coordinate,
+                            span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                        )
+                    }
+            }
+            // END Map
+
+            
+            
+        }
     }
 }
 
